6ca5290d39d06d9774426047826f779a158b9b37
hasankshaik <hasan.kshaik@gmail.com>
2016-07-22 00:23:01 +0100

    inti 3

--- fileprocessor/src/main/java/uk/processor/FileProcessor.java
+++ fileprocessor/src/main/java/uk/processor/FileProcessor.java
@@ -2,6 +2,7 @@ package uk.processor;
 
 import java.io.BufferedReader;
 import java.io.ByteArrayInputStream;
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
@@ -18,6 +19,7 @@ import java.util.zip.ZipInputStream;
 
 import javax.xml.XMLConstants;
 import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
 import javax.xml.bind.Unmarshaller;
 import javax.xml.transform.stream.StreamSource;
 import javax.xml.validation.Schema;
@@ -29,82 +31,75 @@ import org.xml.sax.SAXException;
 import uk.processor.schema.Documents;
 
 public class FileProcessor {
-    private static final String resource = "D:/MOJ/poc/fileprocessor/src/main/resources/";
-    private static final String PNLD_XSD_SCHEMA_LOCATION = resource + "HDDocumentSchema.xsd";
-    private static final String PNLD_DATA_FILES_LOCATION = resource + "H1.xml";
+	private static final String resource = "F:/DEVELOPEMENT/MOJ/poc/fileprocessor/src/main/resources/";
+	private static final String PNLD_XSD_SCHEMA_LOCATION = resource + "HDocumentSchema.xsd";
+	private static final String PNLD_DATA_FILES_LOCATION = resource + "H1.xml";
 
-    public static void main(String... args) throws Exception {
-        readFromZip();
-        // new FileProcessor().parseAndValidatePnld();
-    }
+	public static void main(String... args) throws Exception {
+		// readFromZip();
+		new FileProcessor().someMethod();
+	}
 
-    private final ClassLoader classLoader = getClass().getClassLoader();
+	private final ClassLoader classLoader = getClass().getClassLoader();
 
+	public void someMethod() throws JAXBException {
+		File file = new File(PNLD_DATA_FILES_LOCATION);
+		JAXBContext jaxbContext = JAXBContext.newInstance(Documents.class);
 
-    public Set<Documents> parseAndValidatePnld() throws Exception {
-        InputStream pnldFilesInputStream =
-                        classLoader.getResourceAsStream(PNLD_DATA_FILES_LOCATION);
+		Unmarshaller jaxbUnmarshaller = jaxbContext.createUnmarshaller();
+		Documents customer = (Documents) jaxbUnmarshaller.unmarshal(file);
+		System.out.println(customer.getDocument().getPnldref());
+	}
 
-        BufferedReader reader = new BufferedReader(new InputStreamReader(pnldFilesInputStream));
+	public Set<Documents> parseAndValidatePnld() throws Exception {
 
-        Set<Documents> parsedPnld = new HashSet<>();
-        String currentFile;
+		Set<Documents> parsedPnld = new HashSet<>();
 
-        while ((currentFile = reader.readLine()) != null) {
-            JAXBContext context = JAXBContext.newInstance(Documents.class);
-            Unmarshaller unmarshaller = context.createUnmarshaller();
+		JAXBContext context = JAXBContext.newInstance(Documents.class);
+		Unmarshaller unmarshaller = context.createUnmarshaller();
 
-            SchemaFactory factory = SchemaFactory.newInstance(XMLConstants.W3C_XML_SCHEMA_NS_URI);
-            Schema schema = factory.newSchema(new StreamSource(
-                            classLoader.getResourceAsStream(PNLD_XSD_SCHEMA_LOCATION)));
-            Validator validator = schema.newValidator();
+		SchemaFactory factory = SchemaFactory.newInstance(XMLConstants.W3C_XML_SCHEMA_NS_URI);
+		Schema schema = factory.newSchema(new StreamSource(new FileInputStream(PNLD_XSD_SCHEMA_LOCATION)));
+		Validator validator = schema.newValidator();
 
-            StreamSource file = null;
+		StreamSource file = null;
 
-            try {
-                file = new StreamSource(classLoader.getResourceAsStream(
-                                String.format("%s/%s", PNLD_DATA_FILES_LOCATION, currentFile)));
-                validator.validate(file);
-                Documents document = (Documents) unmarshaller.unmarshal(new StreamSource(
-                                classLoader.getResourceAsStream(String.format("%s/%s",
-                                                PNLD_DATA_FILES_LOCATION, currentFile))));
-                parsedPnld.add(document);
-            } catch (SAXException saxException) {
-                throw new Exception(
-                                String.format("Error validating PNLD file %s against provided XSD",
-                                                file.getPublicId()));
-            } catch (IOException ioException) {
-                throw new Exception(String.format("Error reading file %s", file.getPublicId()));
-            }
-        }
+		try {
+			file = new StreamSource(new FileInputStream(PNLD_DATA_FILES_LOCATION));
+			validator.validate(file);
+			Documents document = (Documents) unmarshaller.unmarshal(file);
+			parsedPnld.add(document);
+		} catch (SAXException saxException) {
+			throw new Exception(
+					String.format("Error validating PNLD file %s against provided XSD", file.getPublicId()));
+		} catch (IOException ioException) {
+			throw new Exception(String.format("Error reading file %s", file.getPublicId()));
+		}
 
-        // reader.close();
-        return parsedPnld;
-    }
+		// reader.close();
+		return parsedPnld;
+	}
 
+	private static void readFromZip() throws FileNotFoundException, IOException, UnsupportedEncodingException {
+		String fileName = "D:/MOJ/_DownloadsAndData/Simple.zip";
+		FileInputStream fis = new FileInputStream(fileName);
+		byte[] data = Files.readAllBytes(Paths.get(fileName));
+		final String encoded = Base64.getEncoder().encodeToString(data);
+		// System.out.println(" => " + encoded);
+		byte[] decode = Base64.getDecoder().decode(encoded);
+		InputStream myInputStream = new ByteArrayInputStream(decode);
+		ZipInputStream zin = new ZipInputStream(myInputStream);
 
-
-    private static void readFromZip()
-                    throws FileNotFoundException, IOException, UnsupportedEncodingException {
-        String fileName = "D:/MOJ/_DownloadsAndData/Simple.zip";
-        FileInputStream fis = new FileInputStream(fileName);
-        byte[] data = Files.readAllBytes(Paths.get(fileName));
-        final String encoded = Base64.getEncoder().encodeToString(data);
-        // System.out.println(" => " + encoded);
-        byte[] decode = Base64.getDecoder().decode(encoded);
-        InputStream myInputStream = new ByteArrayInputStream(decode);
-        ZipInputStream zin = new ZipInputStream(myInputStream);
-
-        ZipEntry ze;
-        while ((ze = zin.getNextEntry()) != null) {
-            if (!ze.isDirectory()) {
-                System.out.println("======== Unzipping == " + ze.getName());
-                for (int c = zin.read(); c != -1; c = zin.read()) {
-                    System.out.write(c);
-                }
-            }
-            zin.closeEntry();
-        }
-        zin.close();
-    }
+		ZipEntry ze;
+		while ((ze = zin.getNextEntry()) != null) {
+			if (!ze.isDirectory()) {
+				System.out.println("======== Unzipping == " + ze.getName());
+				for (int c = zin.read(); c != -1; c = zin.read()) {
+					System.out.write(c);
+				}
+			}
+			zin.closeEntry();
+		}
+		zin.close();
+	}
 }
--- fileprocessor/src/main/java/uk/processor/FileProcessorInAction.java
+++ fileprocessor/src/main/java/uk/processor/FileProcessorInAction.java
@@ -1,6 +1,8 @@
 package uk.processor;
 
 import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.InputStream;
 import java.nio.file.Files;
@@ -13,6 +15,7 @@ import java.util.zip.ZipInputStream;
 
 import javax.xml.XMLConstants;
 import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
 import javax.xml.bind.Unmarshaller;
 import javax.xml.transform.stream.StreamSource;
 import javax.xml.validation.Schema;
@@ -24,67 +27,73 @@ import org.xml.sax.SAXException;
 import uk.processor.schema.Documents;
 
 public class FileProcessorInAction {
-    private static final String resource = "D:/MOJ/poc/fileprocessor/src/main/resources/";
-    private static final String PNLD_XSD_SCHEMA_LOCATION = resource + "HDocumentSchema.xsd";
-    private static final String PNLD_DATA_FILES_LOCATION = resource + "H1.xml";
+	private static final String resource = "F:/DEVELOPEMENT/MOJ/poc/fileprocessor/src/main/resources/";
+	private static final String PNLD_XSD_SCHEMA_LOCATION = resource + "HDocumentSchema.xsd";
+	private static final String PNLD_DATA_FILES_LOCATION = resource + "H1.xml";
+	private static final String FILES_LOCATION = resource + "Simple.zip";
 
-    public static void main(String... args) throws Exception {
-        FileProcessorInAction fileProcessorInAction = new FileProcessorInAction();
-        fileProcessorInAction.readFromZip();
-        // new FileProcessor().parseAndValidatePnld();
-    }
+	public static void main(String... args) throws Exception {
+		FileProcessorInAction fileProcessorInAction = new FileProcessorInAction();
+		fileProcessorInAction.readFromZip();
+		// new FileProcessor().parseAndValidatePnld();
+	}
 
-    public Documents parseAndValidatePnld(InputStream inStream) throws Exception {
+	public Documents parseAndValidatePnld(InputStream inStream) throws Exception {
 
+		JAXBContext context = JAXBContext.newInstance(Documents.class);
+		Unmarshaller unmarshaller = context.createUnmarshaller();
 
+		SchemaFactory factory = SchemaFactory.newInstance(XMLConstants.W3C_XML_SCHEMA_NS_URI);
+		Schema schema = factory.newSchema(new StreamSource(new FileInputStream(PNLD_XSD_SCHEMA_LOCATION)));
+		Validator validator = schema.newValidator();
 
-        JAXBContext context = JAXBContext.newInstance(Documents.class);
-        Unmarshaller unmarshaller = context.createUnmarshaller();
+		StreamSource file = null;
 
-        SchemaFactory factory = SchemaFactory.newInstance(XMLConstants.W3C_XML_SCHEMA_NS_URI);
-        Schema schema = factory
-                        .newSchema(new StreamSource(new FileInputStream(PNLD_XSD_SCHEMA_LOCATION)));
-        Validator validator = schema.newValidator();
+		try {
+			file = new StreamSource(inStream);
+			validator.validate(file);
+			return (Documents) unmarshaller.unmarshal(new StreamSource(inStream));
+		} catch (SAXException saxException) {
+			throw new Exception(
+					String.format("Error validating PNLD file %s against provided XSD", file.getPublicId()));
+		}
 
-        StreamSource file = null;
+		// reader.close();
+	}
+	public Documents someMethod(InputStream is) throws JAXBException {
+		File file = new File(PNLD_DATA_FILES_LOCATION);
+		JAXBContext jaxbContext = JAXBContext.newInstance(Documents.class);
 
-        try {
-            file = new StreamSource(inStream);
-            validator.validate(file);
-            return (Documents) unmarshaller.unmarshal(new StreamSource(inStream));
-        } catch (SAXException saxException) {
-            throw new Exception(String.format("Error validating PNLD file %s against provided XSD",
-                            file.getPublicId()));
-        }
+		Unmarshaller jaxbUnmarshaller = jaxbContext.createUnmarshaller();
+		Documents customer = (Documents) jaxbUnmarshaller.unmarshal(is);
+		System.out.println(customer.getDocument().getPnldref());
+		return customer;
+	}
+	private void readFromZip() throws Exception {
+		FileInputStream fis = new FileInputStream(FILES_LOCATION);
+		byte[] data = Files.readAllBytes(Paths.get(FILES_LOCATION));
+		final String encoded = Base64.getEncoder().encodeToString(data);
+		// System.out.println(" => " + encoded);
+		byte[] decode = Base64.getDecoder().decode(encoded);
+		InputStream myInputStream = new ByteArrayInputStream(decode);
+		ZipInputStream zin = new ZipInputStream(myInputStream);
 
-        // reader.close();
-    }
-
-
-
-    private void readFromZip() throws Exception {
-        String fileName = "D:/MOJ/_DownloadsAndData/Simple.zip";
-        FileInputStream fis = new FileInputStream(fileName);
-        byte[] data = Files.readAllBytes(Paths.get(fileName));
-        final String encoded = Base64.getEncoder().encodeToString(data);
-        // System.out.println(" => " + encoded);
-        byte[] decode = Base64.getDecoder().decode(encoded);
-        InputStream myInputStream = new ByteArrayInputStream(decode);
-        ZipInputStream zin = new ZipInputStream(myInputStream);
-
-        ZipEntry ze;
-        Set<Documents> parsedPnld = new HashSet<>();
-        while ((ze = zin.getNextEntry()) != null) {
-            if (!ze.isDirectory()) {
-                System.out.println("======== Unzipping == " + ze.getName());
-                // for (int c = zin.read(); c != -1; c = zin.read()) {
-                // System.out.write(c);
-                // }
-                Documents x = parseAndValidatePnld(zin);
-                System.out.println(x);
-            }
-            zin.closeEntry();
-        }
-        zin.close();
-    }
+		ZipEntry ze;
+		Set<Documents> parsedPnld = new HashSet<>();
+		while ((ze = zin.getNextEntry()) != null) {
+			if (!ze.isDirectory()) {
+				System.out.println("======== Unzipping == " + ze.getName());
+				ByteArrayOutputStream out = new ByteArrayOutputStream();
+				for (int c = zin.read(); c != -1; c = zin.read()) {
+//					System.out.write(c);
+					out.write(c);
+				}
+				InputStream is = new ByteArrayInputStream(out.toByteArray());
+				Documents x = someMethod(is);
+				System.out.println(x);
+			}
+			zin.closeEntry();
+		}
+		zin.close();
+	}
 }
